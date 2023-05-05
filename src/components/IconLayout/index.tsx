559f60d1b35b7e94efc4f1574ca9b609f333aac2
Eri4ka <navyhasanov@gmail.com>
2023-05-05 12:12:01 +0500

    header

--- src/components/IconLayout/index.tsx
+++ src/components/IconLayout/index.tsx
@@ -1,15 +1,23 @@
+import cl from 'classnames';
 import { FC, ReactElement } from 'react';
 
 import styles from './styles.module.scss';
 
+export enum IconSize {
+  s = 'icon_s',
+  m = 'icon_m',
+  l = 'icon_l',
+}
+
 type Props = {
   icon: ReactElement;
+  size?: IconSize;
   onClick?: () => void;
 };
 
-export const IconLayout: FC<Props> = ({ icon, onClick }) => {
+export const IconLayout: FC<Props> = ({ icon, size = IconSize.s, onClick }) => {
   return (
-    <span className={styles.icon} onClick={onClick}>
+    <span className={cl(styles.icon, styles[size])} onClick={onClick}>
       {icon}
     </span>
   );
