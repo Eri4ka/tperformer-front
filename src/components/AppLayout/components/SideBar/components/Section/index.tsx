ec9efb02d8a0d4db31add4f0287a30f72bc1e0cf
alexAskerko <askerko50@gmail.com>
2023-10-10 20:13:17 +0300

    fix sidebar

--- src/components/AppLayout/components/SideBar/components/Section/index.tsx
+++ src/components/AppLayout/components/SideBar/components/Section/index.tsx
@@ -1,7 +1,7 @@
-import { FC, ReactElement } from 'react';
-import { Link } from 'react-router-dom';
+import {FC, ReactElement} from 'react';
+import {Link, useLocation} from 'react-router-dom';
 
-import { IconLayout, IconSize } from '@/components/IconLayout';
+import {IconLayout, IconSize} from '@/components/IconLayout';
 
 import styles from './styles.module.scss';
 
@@ -12,8 +12,11 @@ type Props = {
 };
 
 export const Section: FC<Props> = ({ href, icon, text }) => {
+
+    const location = useLocation();
+    const activeLink=location.pathname===href?styles.section + " " +styles.sectionActive:styles.section
   return (
-    <Link to={href} className={styles.section}>
+    <Link to={href} className={activeLink}>
       <IconLayout icon={icon} size={IconSize.m} className={styles.sectionIcon} />
       <span className={styles.sectionText}>{text}</span>
     </Link>
